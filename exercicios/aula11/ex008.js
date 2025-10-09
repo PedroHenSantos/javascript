ef41805a30ec75d95f7fec193666434f3d3a5221
Pedro Henrique dos Santos <122707329+PedroHenSantos@users.noreply.github.com>
2025-10-09 19:00:17 -0300

    upando ex009.html teste com condicoes

--- exercicios/aula11/ex008.js
+++ exercicios/aula11/ex008.js
@@ -1,2 +1,8 @@
-var vel  = 60.5
+var vel  = 61
 console.log(`A velocidade do seu carro é ${vel} KM/H`)
+if (vel > 60) {
+    console.log('Voce ultrapassou a velocidade e será multado')
+} else {
+    console.log('Velocidade normal, prossiga')
+}
+console.log('Dirija sempre usando cinto')
